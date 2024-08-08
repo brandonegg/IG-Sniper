fb476a12c53c715b6bdc5a9afbbca66f67d09b2f
Brandon Egger <me@brandonegger.com>
2024-08-07 22:07:17 -0500

    update change title for unix systems

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
-	"unsafe"
 
 	"github.com/dlclark/regexp2"
 	"github.com/gookit/color"
@@ -207,19 +205,8 @@ func readAcc() Account {
 	return obj
 }
 func changeTitle(title string) (int, error) {
-	handle, err := syscall.LoadLibrary("Kernel32.dll")
-	if err != nil {
-		return 0, err
-	}
-	defer syscall.FreeLibrary(handle)
-	proc, err := syscall.GetProcAddress(handle, "SetConsoleTitleW")
-
-	if err != nil {
-		return 0, err
-	}
-
-	r, _, err := syscall.Syscall(proc, 1, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))), 0, 0)
-	return int(r), err
+	fmt.Printf("\033]0;%s\007", title)
+	return 0, nil
 }
 
 func printLogo() {
